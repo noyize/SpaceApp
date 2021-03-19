66447321061ea9d48b3e494cc62816d090c6c984
100441 <noyal.jose@macomsolutions.com>
2021-03-19 21:57:10 +0530

    Initial commit

---
+++ app/src/main/java/com/noyal/spaceapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.noyal.spaceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noyal.spaceapp.databinding.ActivityMainBinding
+import dev.chrisbanes.insetter.Insetter
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Insetter.CONSUME_AUTO
+    }
+}
